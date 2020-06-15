ba664f8527f9425ed3f6ad6f8e2f6d53657849cc
Joao Sousa <joao.rsousa23@gmail.com>
2020-06-15 16:56:21 +0930

    first commit

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+
+app.get('/users', (request, response) => {
+  console.log('listagem de usuarios')
+
+  response.json([
+    'Joao',
+    'antonio'
+  ])
+})
+
+app.listen(3333);
